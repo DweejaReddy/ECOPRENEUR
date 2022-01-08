548212c1b0c1045a6f0fb67b924217be7aed452d
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2022-01-09 00:52:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <title>Ecopreneur</title>
+</head>
+<body>
+  <?php include('includes/header.php')?>
+
+    <section id="home" class="d-none d-lg-block" style="margin-bottom: 13vw;">
+        <div class="eco">ECOPRENEUR</div>
+        <div class="subhead py-1">SUSTAIN THE FUTURE WITH INNOVATION</div>
+        <div class="para">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span></div>
+        <div class="text-center">
+        <a class="btn btn1" href="#" target="_blank" role="button">Download Brochure</a>
+        <a class="btn btn2" href="#" target="_blank" role="button">Register Now</a>
+      </div>
+    </section>
+
+      <section class="d-none d-md-block bg" id="about">
+        <div class="container about align-items-center">
+          <div class="heading text-center">ABOUT THE EVENT</div>
+          <div class="row align-items-center">
+            <div class="col-1"></div>
+            <div class="col-4"><img style="background-color: #fff;border-radius: 20px;" src="img/logo.png" alt="" width="300px" ></div>
+            <div class="col-6 text-left para1">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span> by deducing a proper technical solution to an issue native to their institute's city and creating their own business model out of it. <br><br>Ecopreneur is chance to represent your NIT, it is an opportunity to solve a real world problem, a platform to represent a vision in front of notable people, a medium to win unimaginable prizes and recognition and a direction to maybe even get a fixed career path to follow out of the plan you create!</div>
+            <a class="btn btn3 py-2 px-3 mr-3" href="#" target="_blank" role="button">Download Brochure</a>
+            <a class="btn btn4 px-4 py-2 mx-2 " href="#" target="_blank" role="button">Register Now</a>
+          </div>
+        </div>
+
+        <div class="container align-items-center" id="struct">
+          <div class="heading text-center px-4 my-4">EVENT STRUCTURE</div>
+          <div class="row align-items-center">
+          <div class="col-1"></div>
+          <div class="col-2"><img src="img/circle.png" alt=""></div>
+          <div class="event my-5 col-8" style="height: 16vw;">
+            <h4 class="text-left text-light font-weight-bold ml-3">PHASE 1: Ecowiz</h4>
+            <br>
+                  <p class="text-left text-light ml-3" >A quiz centered around the topic of ecological sustainability will be conducted in all the participating NITS. Top 5 candidates from each college will be chosen to form a team and represent their respective NIT.</p>
+          </div>
+        </div>
+
+        <div class="bord"></div>
+
+        <div class="row align-items-center">
+          <div class="col-1"></div>
+          <div class="col-2"><img src="img/circle.png" alt=""></div>
+          <div class="event my-5 col-8" style="height: 20vw;">
+            <h4 class="text-left text-light font-weight-bold ml-3">PHASE 2: Drafting of the Solution and Business Model</h4>
+            <br>
+            <p class="text-left text-light ml-3">
+                In this intermediate phase, two problem statements will be sent to each team which relate to the local environmental problems in the region where their college is situated. Out of the two, teams will be required to select any one problem statement and build a business model around it. The time duration for this phase will be one month. The teams will have to send us their business models within the stipulated time limit.
+            </p>
+          </div>
+
+          <div class="bord1"></div>
+          <div class="row align-items-center">
+            <div class="col-1"></div>
+            <div class="col-2"><img src="img/circle.png" alt=""></div>
+            <div class="event my-5 col-8" style="height: 20vw;">
+              <h4 class="text-left text-light font-weight-bold ml-3">PHASE 3: Presenting and Pitching</h4>
+              <br>
+              <p class="text-left text-light ml-3">
+                  The 3rd phase will take place in Consortium '22. In this round, the best performing teams, shortlisted on the basis of their business models, will be invited to the VNIT campus. These teams will have to pitch in front of a panel of judges convincing how their business model would be an efficient step to bring about a positive change to solve the environmental problems faced in that region. Based on the worthiness and impact of the idea, the judges will then choose a winner.
+              </p>
+            </div>
+        </div>
+        </div>
+
+        <div class="container about align-items-center my-5" style="height: 35vw;" >
+          <div class="heading text-center">ASSOCIATION</div>
+          <div class="row align-items-center">
+            <div class="col-1"></div>
+            <div class="col-4"><img style="background-color:#fff;border-radius: 20px; padding:5vw 2vw;" src="img/pnaf.jpg" alt="" width="300px" ></div>
+            <div class="col-6 text-left para1" style="margin: auto;">Ecopreneur is proudly conducted in association with the PAN NIT ALUMNI FORUM(PNAF), an umbrella organization, covering alumni associations and thereby alumni of all NITs, to evolve a brand "NIT" that would provide strong fraternity.<br><br> PNAF and E-CELL VNIT share common interests like innovation and entrepreneurship along with holistic development for responsible citizens</div>
+          </div>
+       
+      </section>
+
+
+
+      <!-- MOBILE VERSION -->
+
+      <section id="home_mob" class="d-block d-lg-none" style="margin-bottom: 85vw;">
+        <div class="row align-items-center">
+        <div class="col-2"><img style="margin-top: 10vw;" width="140vw" height="270vh" src="img/eco.png" alt=""></div>
+        <div class="eco">ECOPRENEUR</div>
+        <div class="subhead py-1">SUSTAIN THE FUTURE WITH INNOVATION</div>
+        <div class="para">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span></div>
+        <div class="text-center">
+        <a class="btn btn1 ml-5" href="#" target="_blank" role="button">Download Brochure</a>
+        <a class="btn btn2" href="#" target="_blank" role="button">Register Now</a>
+      </div>
+    </div>
+    </section>
+
+      <section class="d-block d-md-none bg" id="about_mob">
+        <div class="container about align-items-center my-5" style="width: 95vw;">
+          <div class="heading text-center my-4">ABOUT THE EVENT</div>
+          <div class="row align-items-center">
+            <div class="col-md-4 text-center"><img style="background-color: #fff;border-radius: 20px;" src="img/logo.png" alt="" width="200px" ></div>
+            <div class="col text-center para1" style="font-size: 3.7vw;margin-top: 5vw;">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span> by deducing a proper technical solution to an issue native to their institute's city and creating their own business model out of it. <br><br>Ecopreneur is chance to represent your NIT, it is an opportunity to solve a real world problem, a platform to represent a vision in front of notable people, a medium to win unimaginable prizes and recognition and a direction to maybe even get a fixed career path to follow out of the plan you create!</div>
+            <div class="row text-center">
+            <a class="btn btn3 py-2 px-5 my-4 mx-5 col" href="#" target="_blank" role="button">Download Brochure</a>
+            <a class="btn btn4 px-5 py-2 mx-5 col" href="#" target="_blank" role="button">Register Now</a>
+          </div>
+        </div>
+        </div>
+
+
+        <div class="container align-items-center" id="struct_mob">
+          <div class="heading text-center">EVENT STRUCTURE</div>
+          <div class="row align-items-center my-4">
+            <div class="col-1">
+              <img src="img/circle.png" alt="">
+            </div>
+            <div class="col-3"></div>
+            <div class="col-7 event" style="height: 65vw;">
+              <h4 class="text-left text-light font-weight-bold"style="font-size: 5vw;">PHASE 1: Ecowiz</h4>
+                  <p class="text-left text-light" style="font-size: 3.7vw;">A quiz centered around the topic of ecological sustainability will be conducted in all the participating NITS. Top 5 candidates from each college will be chosen to form a team and represent their respective NIT.</p>
+            </div>
+          </div>
+
+          <div class="bord"></div>
+
+          <div class="row align-items-center my-4">
+            <div class="col-1">
+              <img src="img/circle.png" alt="">
+            </div>
+            <div class="col-3"></div>
+            <div class="col-7 event" style="height: 110vw;">
+              <h4 class="text-left text-light font-weight-bold"style="font-size: 5vw;">PHASE 2: Drafting of the Solution and Business Model</h4>
+                  <p class="text-left text-light" style="font-size: 3.7vw;"> In this intermediate phase, two problem statements will be sent to each team which relate to the local environmental problems in the region where their college is situated. Out of the two, teams will be required to select any one problem statement and build a business model around it. The time duration for this phase will be one month. The teams will have to send us their business models within the stipulated time limit.</p>
+            </div>
+          </div>
+
+          <div class="bord1"></div>
+
+          <div class="row align-items-center my-4">
+            <div class="col-1">
+              <img src="img/circle.png" alt="">
+            </div>
+            <div class="col-3"></div>
+            <div class="col-7 event" style="height: 115vw;">
+              <h4 class="text-left text-light font-weight-bold"style="font-size: 5vw;">PHASE 3: Presenting and Pitching</h4>
+                  <p class="text-left text-light" style="font-size: 3.7vw;"> The 3rd phase will take place in Consortium '22. In this round, the best performing teams, shortlisted on the basis of their business models, will be invited to the VNIT campus. These teams will have to pitch in front of a panel of judges convincing how their business model would be an efficient step to bring about a positive change to solve the environmental problems faced in that region. Based on the worthiness and impact of the idea, the judges will then choose a winner.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="container about1 align-items-center" style="width: 95vw;">
+          <div class="heading text-center my-4">ASSOCIATION</div>
+          <div class="row align-items-center">
+            <div class="col-md-4 text-center"><img style="background-color: #fff;border-radius: 20px;padding: 7vw 4vw;" src="img/pnaf.jpg" alt="" width="250px" ></div>
+            <div class="col text-center para1" style="font-size: 3.7vw;margin-top:7vw;">Ecopreneur is proudly conducted in association with the PAN NIT ALUMNI FORUM(PNAF), an umbrella organization, covering alumni associations and thereby alumni of all NITs, to evolve a brand "NIT" that would provide strong fraternity.<br><br> PNAF and E-CELL VNIT share common interests like innovation and entrepreneurship along with holistic development for responsible citizens</div>
+        </div>
+        </div>
+      </section>
+  <?php include('includes/footer.php')?>
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  crossorigin="anonymous"
+  ></script>
+  <script
+  src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+  integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+  crossorigin="anonymous"
+  ></script>
+  <script
+  src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+  integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+  crossorigin="anonymous"
+  ></script>
+</body>
+</html>
