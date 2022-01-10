299f76a9e1f57db6aa2e29d85edff14950f1cdfe
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2022-01-11 00:39:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,10 +16,9 @@
     </section>
 
     <!-- ABOUT -->
-      <section class="d-none d-md-block bg" id="about">
-        <div class="container about align-items-center">
-          <div class="heading text-center" data-aos="fade-down"
-  data-aos-duration="2000">ABOUT THE EVENT</div>
+      <section class="d-none d-lg-block bg" id="about">
+        <div class="container about align-items-center py-5">
+          <div class="heading text-center">ABOUT THE EVENT</div>
           <div class="row align-items-center">
             <div class="col-1"></div>
             <div class="col-4"><img data-aos="fade-right"
@@ -34,14 +33,18 @@
         </div>
 
         <!-- STRUCTURE -->
-        <div class="container align-items-center" id="struct">
+        <div class="container align-items-center d-lg-block d-none" id="struct">
           <div class="heading text-center px-4 my-4" data-aos="fade-down"
   data-aos-duration="2000">EVENT STRUCTURE</div>
+
+
+
           <div class="row align-items-center">
           <div class="col-1"></div>
-          <div class="col-2"><img src="img/circle.png" alt=""></div>
-          <div class="event my-5 col-8" data-aos="fade-left"
-  data-aos-duration="2000" style="height: 16vw;">
+          <div class="col-2">
+            <img src="img/circle.png" alt=""></div>
+          <div class="event my-5 col-8 py-3" data-aos="fade-left"
+  data-aos-duration="2000" style="height: auto;">
             <h4 class="text-left text-light font-weight-bold ml-3">PHASE 1: Ecowiz</h4>
             <br>
                   <p class="text-left text-light ml-3" >A quiz centered around the topic of ecological sustainability will be conducted in all the participating NITS. Top 5 candidates from each college will be chosen to form a team and represent their respective NIT.</p>
@@ -53,7 +56,7 @@
         <div class="row align-items-center">
           <div class="col-1"></div>
           <div class="col-2"><img src="img/circle.png" alt=""></div>
-          <div class="event my-5 col-8" style="height: 20vw;" data-aos="fade-left"
+          <div class="event my-5 col-8 py-3" style="height: auto;" data-aos="fade-left"
   data-aos-duration="2000">
             <h4 class="text-left text-light font-weight-bold ml-3">PHASE 2: Drafting of the Solution and Business Model</h4>
             <br>
@@ -63,10 +66,11 @@
           </div>
 
           <div class="bord1"></div>
+
           <div class="row align-items-center">
             <div class="col-1"></div>
-            <div class="col-2"><img src="img/circle.png" alt=""></div>
-            <div class="event my-5 col-8" style="height: 20vw;" data-aos="fade-left"
+            <div class="col-2 ml-2"><img src="img/circle.png" alt=""></div>
+            <div class="event my-5 col-8 py-3" style="height: auto;" data-aos="fade-left"
   data-aos-duration="2000">
               <h4 class="text-left text-light font-weight-bold ml-3">PHASE 3: Presenting and Pitching</h4>
               <br>
@@ -75,11 +79,9 @@
               </p>
             </div>
         </div>
-        </div>
-     
 
         <!-- ASSOCIATION -->
-        <div class="container about align-items-center my-5" style="height: 35vw;" >
+        <div class="container about align-items-center my-5 py-5 d-none d-lg-block" style="height: auto;" >
           <div class="heading text-center" data-aos="fade-down"
   data-aos-duration="2000">ASSOCIATION</div>
           <div class="row align-items-center">
@@ -96,28 +98,27 @@
 
       <!-- MOBILE VERSION -->
 <!-- HOME -->
-      <section id="home_mob" class="d-block d-lg-none" style="margin-bottom: 85vw;">
-        <div class="row align-items-center">
-        <div class="col-2"><img style="margin-top: 20vw;" width="140vw" height="240vh" src="img/eco.png" alt=""></div>
-        <div class="eco">ECOPRENEUR</div>
-        <div class="subhead py-1">SUSTAIN THE FUTURE WITH INNOVATION</div>
-        <div class="para">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span></div>
-        <div class="text-center">
-        <a class="btn btn1" data-aos="fade-right"
-  data-aos-duration="2000" href="img/Ecopreneur_brochure.pdf" target="_blank" role="button">Download Brochure</a>
-        <a class="btn btn2" data-aos="fade-left"
-  data-aos-duration="2000" href="register.php" target="_blank" role="button">Register Now</a>
-      </div>
-    </div>
-    </section>
 
+    <section class="d-block d-lg-none mt-4" id="home_mob" >
+      <div class="text-center"><img src="img/eco.png" alt="" height="200vw"></div>
+      <div class="text-center" style="font-size:16vw;font-family: 'Bebas Neue', cursive;color: #316835;margin-top: -4vw;">ECOPRENEUR</div>
+      <div class="text-center px-2" style="margin: -4vw 18vw;color: #fff;background-color: #316835;font-size:2.5vw;">SUSTAIN THE FUTURE WITH INNOVATION</div>
+      <div class="text-center mt-4 mt-md-5" style="color: #316835;font-size: 3.2vw; margin: -4vw 18vw;">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span></div>
+      <div class="row align-items-center">
+        <a class="btn btn1 py-2 mt-4 mt-md-5 col-5 " data-aos="fade-right"
+data-aos-duration="2000" href="img/Ecopreneur_brochure.pdf" target="_blank" role="button">Download Brochure</a>
+        <a class="btn btn2 py-2 col-5 mt-4 mt-md-5" data-aos="fade-left"
+data-aos-duration="2000" href="register.php" target="_blank" role="button">Register Now</a>
+      </div>
+
+    </section>
     <!-- ABOUT -->
-      <section class="d-block d-md-none bg" id="about_mob">
-        <div class="container about align-items-center my-5" style="width: 95vw;">
-          <div class="heading text-center my-4" data-aos="fade-up"
+      <section class="d-block d-lg-none bg" id="about_mob" style="position: relative;margin-top: auto;">
+        <div class="container about align-items-center my-5 py-4" style="width: 95vw;">
+          <div class="heading text-center" data-aos="fade-up"
   data-aos-duration="2000">ABOUT THE EVENT</div>
           <div class="row align-items-center">
-            <div class="col-md-4 text-center"><img data-aos="fade-up"
+            <div class="col-lg-4 col-12 text-center"><img data-aos="fade-up"
   data-aos-duration="2000" style="background-color: #fff;border-radius: 20px;" src="img/logo.png" alt="" width="200px" ></div>
             <div class="col text-center para1" data-aos="fade-up"
   data-aos-duration="2000" style="font-size: 3.7vw;margin-top: 5vw;">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span> by deducing a proper technical solution to an issue native to their institute's city and creating their own business model out of it. <br><br>Ecopreneur is chance to represent your NIT, it is an opportunity to solve a real world problem, a platform to represent a vision in front of notable people, a medium to win unimaginable prizes and recognition and a direction to maybe even get a fixed career path to follow out of the plan you create!</div>
@@ -131,7 +132,7 @@
         </div>
 
       <!-- STRUCTURE -->
-        <div class="container align-items-center" id="struct_mob">
+        <div class="container align-items-center d-block d-lg-none" id="struct_mob">
           <div class="heading text-center" data-aos="fade-up"
   data-aos-duration="2000">EVENT STRUCTURE</div>
           <div class="row align-items-center my-4">
@@ -139,7 +140,7 @@
               <img src="img/circle.png" alt="">
             </div>
             <div class="col-3"></div>
-            <div class="col-7 event" style="height: 65vw;" data-aos="fade-up"
+            <div class="col-7 event py-3" style="height: auto;" data-aos="fade-up"
   data-aos-duration="2000">
               <h4 class="text-left text-light font-weight-bold"style="font-size: 5vw;">PHASE 1: Ecowiz</h4>
                   <p class="text-left text-light" style="font-size: 3.7vw;">A quiz centered around the topic of ecological sustainability will be conducted in all the participating NITS. Top 5 candidates from each college will be chosen to form a team and represent their respective NIT.</p>
@@ -153,7 +154,7 @@
               <img src="img/circle.png" alt="">
             </div>
             <div class="col-3"></div>
-            <div class="col-7 event" style="height: 110vw;" data-aos="fade-up"
+            <div class="col-7 event py-3" style="height: auto;" data-aos="fade-up"
   data-aos-duration="2000">
               <h4 class="text-left text-light font-weight-bold"style="font-size: 5vw;">PHASE 2: Drafting of the Solution and Business Model</h4>
                   <p class="text-left text-light" style="font-size: 3.7vw;"> In this intermediate phase, two problem statements will be sent to each team which relate to the local environmental problems in the region where their college is situated. Out of the two, teams will be required to select any one problem statement and build a business model around it. The time duration for this phase will be one month. The teams will have to send us their business models within the stipulated time limit.</p>
@@ -167,7 +168,7 @@
               <img src="img/circle.png" alt="">
             </div>
             <div class="col-3"></div>
-            <div class="col-7 event" style="height: 115vw;" data-aos="fade-up"
+            <div class="col-7 event py-3" style="height: auto;" data-aos="fade-up"
   data-aos-duration="2000">
               <h4 class="text-left text-light font-weight-bold"style="font-size: 5vw;">PHASE 3: Presenting and Pitching</h4>
                   <p class="text-left text-light" style="font-size: 3.7vw;"> The 3rd phase will take place in Consortium '22. In this round, the best performing teams, shortlisted on the basis of their business models, will be invited to the VNIT campus. These teams will have to pitch in front of a panel of judges convincing how their business model would be an efficient step to bring about a positive change to solve the environmental problems faced in that region. Based on the worthiness and impact of the idea, the judges will then choose a winner.</p>
@@ -177,17 +178,18 @@
         </div>
 
         <!-- ASSOCIATION -->
-        <div class="container about1 align-items-center" style="width: 95vw;">
-          <div class="heading text-center my-4" data-aos="fade-up"
+        <div class="container about1 align-items-center d-lg-none d-block py-4 my-5" style="width: 95vw;">
+          <div class="heading text-center" data-aos="fade-up"
   data-aos-duration="2000">ASSOCIATION</div>
           <div class="row align-items-center">
-            <div class="col-md-4 text-center"><img data-aos="fade-up"
-  data-aos-duration="2000" style="background-color: #fff;border-radius: 20px;padding: 7vw 4vw;" src="img/pnaf.jpg" alt="" width="250px" ></div>
+            <div class="col-lg-4 text-center"><img data-aos="fade-up"
+  data-aos-duration="2000" style="background-color: #fff;border-radius: 20px;padding: 7vw 4vw;" src="img/pnaf.jpg" alt="" width="200vw" ></div>
             <div class="col text-center para1" data-aos="fade-up"
   data-aos-duration="2000" style="font-size: 3.7vw;margin-top:7vw;">Ecopreneur is proudly conducted in association with the PAN NIT ALUMNI FORUM(PNAF), an umbrella organization, covering alumni associations and thereby alumni of all NITs, to evolve a brand "NIT" that would provide strong fraternity.<br><br> PNAF and E-CELL VNIT share common interests like innovation and entrepreneurship along with holistic development for responsible citizens</div>
         </div>
         </div>
       </section>
+      <div class="bg d-block d-lg-none" style="height: 15vw;"></div>
   <?php include('includes/footer.php')?>
 </body>
 </html>
